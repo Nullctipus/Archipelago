1c410b4e4faf0c417d9c1d4690cc2fe790ae9663
Nullctipus <cole.frankland@gmail.com>
2025-02-24 20:12:37 -0600

    ClusterTruck: initial apworld

---
+++ worlds/cluster_truck/Items.py
@@ -0,0 +1,42 @@
+from typing import List, Dict, Set
+from dataclasses import dataclass
+from enum import Enum
+
+base_id = 9002100
+
+class ItemType(Enum):
+    Ability = 0
+    Level = 1
+    Trap = 2
+    Filler = 3
+
+
+@dataclass
+class CTItem:
+    name: str
+    type: ItemType
+
+
+item_list: List[CTItem] = [
+    CTItem("Double Jump", ItemType.Ability),
+    CTItem("Air Dash", ItemType.Ability),
+    CTItem("Jetpack", ItemType.Ability),
+    CTItem("Levitation", ItemType.Ability),
+    CTItem("Grappling Hook", ItemType.Ability),
+    CTItem("Truck Boost", ItemType.Ability),
+    CTItem("Back Truck", ItemType.Ability),
+    CTItem("Trucker Flip", ItemType.Ability),
+    CTItem("Time Slow", ItemType.Ability),
+    CTItem("Portable Truck", ItemType.Ability),
+    CTItem("Trucksolute Zero", ItemType.Ability),
+    CTItem("Epic Mode", ItemType.Ability),
+    CTItem("Supertruck", ItemType.Ability),
+    CTItem("Disrespected Blink", ItemType.Ability),
+
+    CTItem("+1000p",ItemType.Filler),
+    CTItem("Infinite Stamina",ItemType.Filler),
+    CTItem("Impulse",ItemType.Trap),
+    CTItem("Brake",ItemType.Trap),
+]
+
+item_list.extend([CTItem(f"{i//10+1}-{i%10+1}",ItemType.Level) for i in range(90)])
